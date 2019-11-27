e67d06bb16de44e79ff63e18cd98cc7d32ad079e
tokada <theodoreokada@gmail.com>
2019-11-27 16:00:43 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    var magic8Ball = {};
+
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+
+    magic8Ball.askQuestion = function(question){
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+
+        $("#answer").text( answer );
+
+        console.log(question);
+        console.log(answer);
+    };
+
+    var onClick = function() {
+        var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+    };
+
+    $("#questionButton").click( onClick );
+
+});
